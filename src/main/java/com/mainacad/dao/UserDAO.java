cecd2c64b2a548fe340aac9824ee9da0a9b43f58
Antoninadan <mail100@i.ua>
2019-11-26 22:55:52 +0200

    add update and delete methods

--- src/main/java/com/mainacad/dao/UserDAO.java
+++ src/main/java/com/mainacad/dao/UserDAO.java
@@ -2,12 +2,12 @@ package com.mainacad.dao;
 
 import com.mainacad.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.logging.Logger;
 
 public class UserDAO {
+    private static final Logger LOG = Logger.getLogger(UserDAO.class.getName());
+
     public static User save(User user) {
         String sql = "INSERT INTO users " +
                 "(login, password, first_name, last_name, email, phone) " +
@@ -42,41 +42,80 @@ public class UserDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
         return user;
-
-
     }
 
     public static User getById(Integer id) {
         String sql = "SELECT * FROM users WHERE id = ?";
+        User user = new User();
+        int result = 0;
 
-        return null;
+        try (Connection connection = ConnectionToDB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             Statement statement = connection.createStatement()) {
+            preparedStatement.setInt(1, id);
+
+            result = preparedStatement.executeUpdate();
+            if (result == 1) {
+                ResultSet resultSet = statement.executeQuery(sql);
+
+                if (resultSet.first() == true) {
+                    user.setId(resultSet.getInt("id"));
+                    user.setLogin(resultSet.getString("login"));
+                    user.setPassword(resultSet.getString("password"));
+                    user.setFirstName(resultSet.getString("first_name"));
+                    user.setLastName(resultSet.getString("last_name"));
+                    user.setEmail(resultSet.getString("email"));
+                    user.setPhone(resultSet.getString("phone"));
+                }
+                resultSet.close();
+            }
+
+            } catch(SQLException e){
+                e.printStackTrace();
+            }
+            return user;
+        }
+
+        public static User getByLoginAndPassword (String login, String password){
+            String sql = "SELECT * FROM users WHERE login = ? AND password = ?";
+            return null;
+        }
+
+        public static User update (User user){
+            String sql = "UPDATE users SET "
+                    + "login = ?, "
+                    + "password = ?, "
+                    + "first_name = ?, "
+                    + "last_name = ?, "
+                    + "email = ?, "
+                    + "phone = ? "
+                    + "WHERE id = ?";
+            try (Connection connection = ConnectionToDB.getConnection();
+                 PreparedStatement preparedStatement =
+                         connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, user.getLogin());
+                preparedStatement.setString(2, user.getPassword());
+                preparedStatement.setString(3, user.getFirstName());
+                preparedStatement.setString(4, user.getLastName());
+                preparedStatement.setString(5, user.getEmail());
+                preparedStatement.setString(6, user.getPhone());
+                preparedStatement.setInt(7, user.getId());
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return user;
+        }
+
+        public static void delete (Integer id){
+            String sql = "DELETE FROM users WHERE id = ?";
+            try (Connection connection = ConnectionToDB.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, id);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    public static User getByLoginAndPassword(String login, String password) {
-        String sql = "SELECT * FROM users WHERE login = ? AND password = ?";
-
-        return null;
-    }
-
-    public static User update(User user) {
-        String sql = "UPDATE users SET "
-                + "login = ?"
-                + "password = ?"
-                + "first_name = ?"
-                + "last_name = ?"
-                + "email = ?"
-                + "phone = ?"
-                + "WHERE id = ?";
-
-        return user;
-    }
-
-    public static void delete(Integer id) {
-        String sql = "DELETE FROM users WHERE id = ?";
-    }
-
-
-}
--- src/test/java/com/mainacad/dao/UserDAOTest.java
+++ src/test/java/com/mainacad/dao/UserDAOTest.java
@@ -9,9 +9,54 @@ class UserDAOTest {
 
     @Test
     void save() {
-        User user = new User("login", "password", "fisrtname",
+        User user = new User("login", "password", "firstname",
                 "lastname", "email", "+4353456363464");
         UserDAO.save(user);
         assertNotNull(user.getId());
     }
+
+    @Test
+    void update() {
+        User user = new User("login1", "password1", "firstname1",
+                "lastname1", "email1", "phone1");
+        UserDAO.save(user);
+
+        user.setLogin("login_update");
+        user.setPassword("password_update");
+        user.setFirstName("firstname_update");
+        user.setLastName("lastname_update");
+        user.setEmail("email_update");
+        user.setPhone("phone_update");
+
+        UserDAO.update(user);
+    }
+
+    @Test
+    void delete() {
+        User user = new User("login2", "password2", "firstname2",
+                "lastname2", "email2", "phone2");
+        UserDAO.save(user);
+        Integer id = user.getId();
+        UserDAO.delete(id);
+    }
+
+    @Test
+    void getById() {
+        User user = new User("login3", "password3", "firstname3",
+                "lastname3", "email3", "phone3");
+        UserDAO.save(user);
+        Integer id = user.getId();
+
+        User newUser = new User();
+        newUser = UserDAO.getById(id);
+
+        assertEquals(id, newUser.getId());
+        assertEquals("login3", newUser.getLogin());
+        assertEquals("password3", newUser.getPassword());
+        assertEquals("firstname3", newUser.getFirstName());
+        assertEquals("lastname3", newUser.getLastName());
+        assertEquals("email3", newUser.getEmail());
+        assertEquals("phone3", newUser.getPhone());
+    }
+
 }
